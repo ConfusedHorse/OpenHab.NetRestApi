822abd3949f6e6952ba9a91be9161a0373209997
Joerg Flechsig <goergchen@gmail.com>
2017-05-29 03:28:17 +0200

    added UuidService

---
+++ OpenHAB.NetRestApi/Services/UuidService.cs
@@ -0,0 +1,16 @@
+﻿using OpenHAB.NetRestApi.Models;
+using OpenHAB.NetRestApi.RestApi;
+using RestSharp;
+
+namespace OpenHAB.NetRestApi.Services
+{
+    public class UuidService
+    {
+        public string GetUuid()
+        {
+            const string resource = "/uuid";
+            var result = OpenHab.RestClient.ExecuteRequestAsync(Method.GET, resource, requestHeaders: RequestHeader.AcceptPlainText).Result;
+            return result.Content;
+        }
+    }
+}
