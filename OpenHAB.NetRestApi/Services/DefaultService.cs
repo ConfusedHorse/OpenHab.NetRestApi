34988e63454e3907a154fae218543c6205e3d83f
Joerg Flechsig <goergchen@gmail.com>
2017-08-01 02:48:47 +0200

    abandoned DefaultService

--- OpenHAB.NetRestApi/Services/DefaultService.cs
+++ OpenHAB.NetRestApi/Services/DefaultService.cs
@@ -1,7 +1,12 @@
-﻿namespace OpenHAB.NetRestApi.Services
+﻿using System;
+
+namespace OpenHAB.NetRestApi.Services
 {
     public class DefaultService
     {
-        
+        public DefaultService()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
