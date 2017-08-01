8ae4bd61ae4e964113dbff7f212b89c0c02e19c2
Joerg Flechsig <goergchen@gmail.com>
2017-08-01 23:03:29 +0200

    added InboxService

--- OpenHAB.NetRestApi.Client/Program.cs
+++ OpenHAB.NetRestApi.Client/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using Newtonsoft.Json;
-using OpenHAB.NetRestApi.Models.Events;
 using OpenHAB.NetRestApi.RestApi;
 
 namespace OpenHAB.NetRestApi.Client
@@ -18,43 +15,12 @@ namespace OpenHAB.NetRestApi.Client
         static void Main(string[] args)
         {
             var openHab = OpenHab.CreateRestClient(Url, StartEventService);
-            var eventService = openHab.EventService;
-            
-            eventService.ItemStateEventOccured += DebugAnyEventOccured;
-            eventService.ItemStateChanged += DebugAnyEventOccured;
-            eventService.ItemCommandEventOccured += DebugAnyEventOccured;
-            eventService.ItemAdded += DebugAnyEventOccured;
-            eventService.ItemRemoved += DebugAnyEventOccured;
-            eventService.ItemChannelLinkAdded += DebugAnyEventOccured;
-            eventService.ItemChannelLinkRemoved += DebugAnyEventOccured;
+            var inboxService = openHab.InboxService;
 
-            eventService.ThingStatusInfoEventOccured += DebugAnyEventOccured;
-            eventService.ThingStatusInfoChanged += DebugAnyEventOccured;
-            eventService.ThingUpdated += DebugAnyEventOccured;
-            eventService.ThingRemoved += DebugAnyEventOccured;
-
-            eventService.InboxAdded += DebugAnyEventOccured;
-            eventService.InboxRemoved += DebugAnyEventOccured;
-
-            eventService.RuleStatusInfoEventOccured += DebugAnyEventOccured;
-            eventService.RuleAdded += DebugAnyEventOccured;
-            eventService.RuleUpdated += DebugAnyEventOccured;
-            eventService.RuleRemoved += DebugAnyEventOccured;
-
-            eventService.ConfigStatusInfoEventOccured += DebugAnyEventOccured;
-            eventService.PlayUrlEventOccured += DebugAnyEventOccured;
-            eventService.UnknownEventOccured += DebugAnyEventOccured;
+            var inbox = inboxService.GetInbox();
+            var acceptInbox = inboxService.AcceptInbox("validUid:0:0", "somelabel");
 
             Console.ReadLine();
-        }
-
-        private static void DebugAnyEventOccured(object sender, OpenHabEvent eventObject)
-        {
-            Debug.WriteLine(
-                $"{eventObject.Occured}\t\t{eventObject.Type}:\t\t{eventObject.Target}({eventObject.Action})");
-            object payload = ((dynamic) eventObject).Payload;
-            Debug.WriteLine(JsonConvert.SerializeObject(payload));
-            Debug.WriteLine(string.Empty);
         }
     }
 }
--- OpenHAB.NetRestApi/Services/InboxService.cs
+++ OpenHAB.NetRestApi/Services/InboxService.cs
@@ -1,7 +1,67 @@
-﻿namespace OpenHAB.NetRestApi.Services
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenHAB.NetRestApi.Models;
+using OpenHAB.NetRestApi.RestApi;
+using RestSharp;
+
+namespace OpenHAB.NetRestApi.Services
 {
     public class InboxService
     {
+        public List<Inbox> GetInbox()
+        {
+            return GetInboxAsync().Result;
+        }
         
+        public Task<List<Inbox>> GetInboxAsync(CancellationToken token = default(CancellationToken))
+        {
+            const string resource = "/inbox";
+            return OpenHab.RestClient.ExecuteRequestAsync<List<Inbox>>(Method.GET, resource, token: token);
+        }
+
+        public string DeleteInbox(string thingUid)
+        {
+            return DeleteInboxAsync(thingUid).Result.Content;
+        }
+        
+        public Task<IRestResponse> DeleteInboxAsync(string thingUid, CancellationToken token = default(CancellationToken))
+        {
+            var resource = $"/inbox/{thingUid}";
+            return OpenHab.RestClient.ExecuteRequestAsync(Method.DELETE, resource, token: token);
+        }
+
+        public string AcceptInbox(string thingUid, string label)
+        {
+            return AcceptInboxAsync(thingUid, label).Result.Content;
+        }
+        
+        public Task<IRestResponse> AcceptInboxAsync(string thingUid, string label, CancellationToken token = default(CancellationToken))
+        {
+            var resource = $"/inbox/{thingUid}/approve";
+            return OpenHab.RestClient.ExecuteRequestAsync(Method.POST, resource, label, RequestHeader.ContentPlainText, token);
+        }
+
+        public string IgnoreInbox(string thingUid)
+        {
+            return IgnoreInboxAsync(thingUid).Result.Content;
+        }
+        
+        public Task<IRestResponse> IgnoreInboxAsync(string thingUid, CancellationToken token = default(CancellationToken))
+        {
+            var resource = $"/inbox/{thingUid}/ignore";
+            return OpenHab.RestClient.ExecuteRequestAsync(Method.POST, resource, token: token);
+        }
+
+        public string UnignoreInbox(string thingUid)
+        {
+            return UnignoreInboxAsync(thingUid).Result.Content;
+        }
+        
+        public Task<IRestResponse> UnignoreInboxAsync(string thingUid, CancellationToken token = default(CancellationToken))
+        {
+            var resource = $"/inbox/{thingUid}/unignore";
+            return OpenHab.RestClient.ExecuteRequestAsync(Method.POST, resource, token: token);
+        }
     }
 }
